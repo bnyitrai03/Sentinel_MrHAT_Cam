620c34012c4d62c7ead4a12b360757594c72b307
bnyitrai03 <bence.nyitrai@gmail.com>
2024-11-27 11:46:32 +0100

    fixed linting

--- tests/systemTest.py
+++ tests/systemTest.py
@@ -1,8 +1,3 @@
-import pytest
-from unittest.mock import patch
-from gpiozero import CPUTemperature
-from sentinel_mrhat_cam import System
-
 def test_get_cpu_temperature():
-    result = True
-    assert result == True
+    result = 1
+    assert result == 1
