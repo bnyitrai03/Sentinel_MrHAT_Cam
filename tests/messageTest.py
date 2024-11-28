0e71314076fcc329eb8f781dc09d767c731ff147
bnyitrai03 <bence.nyitrai@gmail.com>
2024-11-28 17:51:40 +0100

    fixed linting

--- tests/messageTest.py
+++ tests/messageTest.py
@@ -6,16 +6,16 @@ import base64
 from unittest.mock import MagicMock
 from sentinel_mrhat_cam import MessageCreator
 
+
 # Megy szakdogába :D
 def test_create_base64_image():
     test_instance = MessageCreator(camera=MagicMock(), rtc=MagicMock(), system=MagicMock())
     test_image_array = np.full((100, 100, 3), 128, dtype=np.uint8)
     test_instance._camera.capture.return_value = test_image_array
-    
+
     result = test_instance._create_base64_image()
     assert isinstance(result, str)
     try:
-        Image.open(io.BytesIO( base64.b64decode(result)))
+        Image.open(io.BytesIO(base64.b64decode(result)))
     except Exception as e:
         pytest.fail(f"Image is not encoded error: {e}")
-        
--- tests/statesTest.py
+++ tests/statesTest.py
@@ -1,27 +1,30 @@
 import pytest
 import time
-import pytest
 from unittest.mock import MagicMock
-from sentinel_mrhat_cam import Context, IdleState, SHUTDOWN_THRESHOLD
+from sentinel_mrhat_cam import Context, IdleState
+
 
 def test_log_and_save_execution_time():
     Context.runtime = 0
+
     @Context.log_and_save_execution_time("Test Function")
     def test_function():
         time.sleep(0.5)
-    
+
     test_function()
     assert pytest.approx(Context.runtime, 0.1) == 0.5
+
 
 def test_schedule_sleep_until_next_cycle():
     app_mock = MagicMock()
     test_instance = IdleState()
     waiting_time = 0.1
     period = 0.5
-    
+
     test_instance._schedule_next_cycle(app_mock, period, waiting_time)
     app_mock.reset_runtime.assert_called_once()
     app_mock.set_state.assert_called_once()
+
 
 def test_schedule_shutdown_until_next_cycle():
     app_mock = MagicMock()
@@ -29,6 +32,6 @@ def test_schedule_shutdown_until_next_cycle():
     test_instance = IdleState()
     waiting_time = 10
     period = -1
-    
+
     test_instance._schedule_next_cycle(app_mock, period, waiting_time)
     assert period == -1
